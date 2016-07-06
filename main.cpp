4471c5ab174f302715c2d3b7e97038880a4c8efc
Daniel Laguna <labellson@gmail.com>
2016-07-06 11:46:11 +0200

    Added little OpenMP support

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
-
+#include <queue>
+#include <omp.h>
 #include "openni2_cv_grabber.h"
 
 using namespace std;
@@ -9,20 +10,19 @@ using namespace cv;
 
 int main(int argc, char **argv){
     Openni2_grabber *grabber = new Openni2_grabber();
-    grabber->init_ir_stream();
     grabber->init_depth_stream();
     grabber->init_rgb_stream();
 
-    Mat ir_frame, depth_frame, rgb_frame;
-    int counter = 0;
+    Mat depth_frame, rgb_frame;
+    queue<Mat*> color_buff, depth_buff;
+    int counter_depth = -1, counter_color = -1;
     bool go = true;
     
     //Lets Rock and grab data
-    while(go){
-        grabber->retrieve(ir_frame, Openni2_grabber::IR_IMAGE);
+    /*while(go){
+
         grabber->retrieve(depth_frame, Openni2_grabber::DEPTH_IMAGE);
         grabber->retrieve(rgb_frame, Openni2_grabber::RGB_IMAGE);
-        imshow("ir_raw", ir_frame);
         imshow("depth_raw", depth_frame);
         imshow("rgb_raw", rgb_frame);
         switch(waitKey(30)){
@@ -38,6 +38,45 @@ int main(int argc, char **argv){
                 ss1 << "depth_" << counter << ".png";
                 imwrite(ss1.str(), depth_frame);
                 break;
+        }
+    }*/
+    #pragma omp parallel shared(go, color_buff, depth_buff), num_threads(2)
+    {
+        while(go){
+            #pragma omp sections nowait
+            {
+                #pragma omp section
+                {
+                   grabber->retrieve(depth_frame, Openni2_grabber::DEPTH_IMAGE);
+                   grabber->retrieve(rgb_frame);
+
+                   #pragma omp critical buffer
+                   {
+                        depth_buff.push(&depth_frame);
+                        color_buff.push(&rgb_frame);
+                   }
+                }
+
+                #pragma omp section
+                {
+
+                    if(!depth_buff.empty()){
+                        //Write the image
+                        stringstream ss;
+                        ss << "depth_" << ++counter_depth << ".png";
+                        imwrite(ss.str(), *(depth_buff.front()));
+                        depth_buff.pop();
+                    }
+
+                    if(!color_buff.empty()){
+                        //Write again
+                        stringstream ss;
+                        ss << "rgb_" << ++counter_color << ".png";
+                        imwrite(ss.str(), *(color_buff.front()));
+                        color_buff.pop();
+                    }
+                }
+            }
         }
     }
 }
