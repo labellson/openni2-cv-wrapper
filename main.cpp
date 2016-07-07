e6c689f7b14b4aa578289878da1a0ab2e9d99b84
Daniel Laguna <labellson@gmail.com>
2016-07-07 19:05:10 +0200

    Parallel grabber with OpenMP

--- main.cpp
+++ main.cpp
@@ -3,12 +3,24 @@
 #include <iostream>
 #include <queue>
 #include <omp.h>
+#include <signal.h>
+#include <chrono>
+#include <iomanip>
 #include "openni2_cv_grabber.h"
 
 using namespace std;
 using namespace cv;
 
+bool SIGINT_RAISED = false;
+
+void sigint_handler(int s){
+    cout << "Wait until frames are saved.." << endl;
+    SIGINT_RAISED = true;
+}
+
 int main(int argc, char **argv){
+    signal(SIGINT, sigint_handler);
+
     Openni2_grabber *grabber = new Openni2_grabber();
     grabber->init_depth_stream();
     grabber->init_rgb_stream();
@@ -18,63 +30,52 @@ int main(int argc, char **argv){
     int counter_depth = -1, counter_color = -1;
     bool go = true;
     
-    //Lets Rock and grab data
-    /*while(go){
-
-        grabber->retrieve(depth_frame, Openni2_grabber::DEPTH_IMAGE);
-        grabber->retrieve(rgb_frame, Openni2_grabber::RGB_IMAGE);
-        imshow("depth_raw", depth_frame);
-        imshow("rgb_raw", rgb_frame);
-        switch(waitKey(30)){
-            case 27:
-                go = false;
-                break;
-            case 32:
-                cout << "Saved frame " << ++counter << endl;
-                stringstream ss;
-                ss << "rgb_" << counter << ".png";
-                imwrite(ss.str(), rgb_frame);
-                stringstream ss1;
-                ss1 << "depth_" << counter << ".png";
-                imwrite(ss1.str(), depth_frame);
-                break;
-        }
-    }*/
-    #pragma omp parallel shared(go, color_buff, depth_buff), num_threads(2)
+    #pragma omp parallel shared(go, color_buff, depth_buff), num_threads(8)
     {
         while(go){
             #pragma omp sections nowait
             {
                 #pragma omp section
                 {
-                   grabber->retrieve(depth_frame, Openni2_grabber::DEPTH_IMAGE);
-                   grabber->retrieve(rgb_frame);
+                    if(!SIGINT_RAISED){
+                        grabber->retrieve(depth_frame, Openni2_grabber::DEPTH_IMAGE);
+                        grabber->retrieve(rgb_frame);
 
-                   #pragma omp critical buffer
-                   {
-                        depth_buff.push(&depth_frame);
-                        color_buff.push(&rgb_frame);
-                   }
+                        #pragma omp critical(buffer)
+                        {
+                            depth_buff.push(&depth_frame);
+                            color_buff.push(&rgb_frame);
+                        }
+                    }
                 }
 
                 #pragma omp section
                 {
+                    chrono::system_clock::time_point now = std::chrono::system_clock::now();
+                    time_t time = std::chrono::system_clock::to_time_t(now);
+                    chrono::milliseconds  ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) -
+                    std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch());
 
                     if(!depth_buff.empty()){
                         //Write the image
                         stringstream ss;
-                        ss << "depth_" << ++counter_depth << ".png";
+                        //ss << "depth_" << ++counter_depth << ".png";
+                        ss << "depth_" << put_time(localtime(&time), "%H:%M:%S:") << ms.count() << ".png";
                         imwrite(ss.str(), *(depth_buff.front()));
+                        //#pragma omp atomic
                         depth_buff.pop();
                     }
 
                     if(!color_buff.empty()){
                         //Write again
                         stringstream ss;
-                        ss << "rgb_" << ++counter_color << ".png";
+                        //ss << "rgb_" << ++counter_color << ".png";
+                        ss << "rgb_" << put_time(localtime(&time), "%H:%M:%S:") << ms.count() << ".png";
                         imwrite(ss.str(), *(color_buff.front()));
+                        //#pragma omp atomic
                         color_buff.pop();
                     }
+                    if(SIGINT_RAISED && depth_buff.empty() && color_buff.empty()) go = false;
                 }
             }
         }
